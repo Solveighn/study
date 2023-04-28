860df475eef8d34ddd5a006860f29943e9296374
Anastasia <kholmogorovaaa@mail.cbr.ru>
2023-04-28 10:50:41 +0300

    updated postamat_delivery.js

--- 7.1/postamat_delivery.js
+++ 7.1/postamat_delivery.js
@@ -1,12 +1,17 @@
-let postamat =  [null, null, null, "4 - ваша посылка" , null];
+let postamat =  [null, null, null, "A001DFX0" , null];
 let customer = {
     sum: 100,
     codeSMS: "A001DFX0",
     textSMS: "… ваша посылка находится в ячейке под номером 4"
 }
-if (customer.codeSMS === "A001DFX0") {
+let getDelivery = false;
+
+for(let value of postamat){
+if (customer.codeSMS === value) {
 console.log(`Вы получили посылку из ячейки 4 и ваш счет составляет: ${customer.sum} р.`)
-}
-else {
-console.log("Неверный код")
+getDelivery = true;
+}}
+
+if (getDelivery == false) {
+    console.log("Неверный код")
 }
