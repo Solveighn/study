e13a030cd32724f42b8aac597de6c12e3e490a39
Anastasia <kholmogorovaaa@mail.cbr.ru>
2023-07-08 16:39:15 +0300

    15.4 changed

--- 15.4/cypress.config.js
+++ 15.4/cypress.config.js
@@ -1,10 +1,8 @@
-const { defineConfig } = require("cypress");
-
 module.exports = defineConfig({
   projectId: "qncci3",
   pageLoadTimeout: 120000,
   e2e: {
-    baseUrl: "https://staging.lpitko.ru/",
+    baseUrl: "https://staging.lpitko.ru",
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
