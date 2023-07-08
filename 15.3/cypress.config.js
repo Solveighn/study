6067d87b275d4c77f32ebd1758fd29144e309c70
Anastasia <kholmogorovaaa@mail.cbr.ru>
2023-07-08 11:56:00 +0300

    15.3 changed

--- 15.3/cypress.config.js
+++ 15.3/cypress.config.js
@@ -2,6 +2,7 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   projectId: "b1db4g",
+  pageLoadTimeout: 120000,
   e2e: {
     baseUrl: "https://staging.lpitko.ru/",
     setupNodeEvents(on, config) {
