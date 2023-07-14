da054533ad96320422a124b9ed3ec84e4fb1af76
Anastasia <kholmogorovaaa@mail.cbr.ru>
2023-07-14 15:42:18 +0300

    16.2 updated

--- 16.2/cypress/e2e/secretSanta_test.cy.js
+++ 16.2/cypress/e2e/secretSanta_test.cy.js
@@ -1,21 +1,21 @@
 describe("SecretSanta_tests", () => {
     beforeEach("Authorization", () => {
-        cy.visit("/");
-        cy.get("#root > div.layout-1 > section.layout-1__header-wrapper-fixed > header > section > div > a > div > div > span").click();
-        cy.get("#root > div.layout-1 > section.layout-1__main-wrapper > div.layout-1__main > section > div > section > div > div.form-auth__main > div:nth-child(3) > input").type("lisenysh1@yandex.ru");
-        cy.get("#root > div.layout-1 > section.layout-1__main-wrapper > div.layout-1__main > section > div > section > div > div.form-auth__main > div:nth-child(4) > input").type("123456");
-        cy.get("#root > div.layout-1 > section.layout-1__main-wrapper > div.layout-1__main > section > div > section > div > div.form-auth__button > div").click();
+        cy.Auth();
     });
     it("Check boxes", () => {
         cy.checkLink("#root > div.layout-1 > section.layout-1__header-wrapper-fixed > header > section > div > div > a:nth-child(1) > div > div.header-item__text > span", "account/boxes");
+        cy.checkTitle("head > title", "Тайный Санта | мои коробки");
     });
     it("Check CreateBox", () => {
         cy.checkLink("#root > div.layout-1 > section.layout-1__main-wrapper > div.layout-1__main > section > div > section > div > div.home-page-buttons > a:nth-child(1)", "box/new");
+        cy.checkTitle("head > title", "Тайный Санта | создать коробку");
     });
     it("Check FastToss", () => {
         cy.checkLink("#root > div.layout-1 > section.layout-1__main-wrapper > div.layout-1__main > section > div > section > div > div.home-page-buttons > a:nth-child(2)", "randomizer");
+        cy.checkTitle("head > title", "Тайный Санта | быстрая жеребьевка");
     });   
     it("Check Account", () => {
         cy.checkLink("#root > div.layout-1 > section.layout-1__header-wrapper-fixed > header > section > div > div > a:nth-child(3)", "account");
+        cy.checkTitle("head > title", "Тайный Санта | настройки профиля");
     });       
 })
